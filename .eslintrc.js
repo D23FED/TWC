d28418e67143dd75eb13e67a10fe8c733aca1e03
mehas <ethan.mehas@gmail.com>
2016-04-06 13:27:03 -0400

    Initial eslint install

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "warning",
+            "tab"
+        ],
+        "linebreak-style": [
+            "warning",
+            "windows"
+        ],
+        "quotes": [
+            "warning",
+            "single"
+        ],
+        "semi": [
+            "warning",
+            "always"
+        ]
+    }
+};
