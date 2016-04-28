a599ebcf12ab7cf3fbd28498086ed82dd0bad8d6
mehas <ethan.mehas@gmail.com>
2016-04-28 16:32:43 -0400

    Build styles to support update to CIC (center compare label)

--- src/sandbox/slidePackageSelectIcons/js/page.js
+++ src/sandbox/slidePackageSelectIcons/js/page.js
@@ -1,6 +1,6 @@
 // Packages - expand to show details
-$('.side-by-side .expandDetails').click(function(){
-  if($(this).parents().nextAll('.detailsExpansion').is(':hidden')){
+$('.side-by-side .expandDetails').click(function() {
+  if ($(this).parents().nextAll('.detailsExpansion').is(':hidden')) {
     $(this).parents().nextAll('.detailsExpansion').slideToggle().addClass('js-active');
     $(this).removeClass('icon-angle-up').addClass('icon-angle-down').addClass('js-active');
     $(this).closest('.package').addClass('js-active');
@@ -9,10 +9,10 @@ $('.side-by-side .expandDetails').click(function(){
     $(this).removeClass('icon-angle-down').addClass('icon-angle-up').removeClass('js-active');
     $(this).closest('.package').removeClass('js-active');
   }
-    return false;
+  return false;
 });
 
-$('.closeDetails').click(function(){
+$('.closeDetails').click(function() {
   $(this).parents('.detailsExpansion').slideToggle().removeClass('js-active');
   $(this).removeClass('js-active');
   $(this).closest('.package').removeClass('js-active');
@@ -22,11 +22,10 @@ $('.closeDetails').click(function(){
 // End expand to show details
 
 // JS to control compare checkbox so it swaps the label to a button
-$(".comparePackage:checkbox").on('click', function(){
-  if($(this).is(":checked")){
+$(".comparePackage:checkbox").on('click', function() {
+  if ($(this).is(":checked")) {
     $(this).closest('.package').addClass('js-compare');
-  }
-  else {
+  } else {
     $(this).closest('.package').removeClass('js-compare');
   }
 });
@@ -35,44 +34,37 @@ $(".comparePackage:checkbox").on('click', function(){
 // Compare Button Maximum Alert
 $(".comparePackage").on("click", function() {
   var compareSelected = $(".comparePackage:checked").length;
-  if($(window).width() > 768){
-    if(compareSelected >= 2) {
-      if(compareSelected > 3) {
+  if ($(window).width() > 768) {
+    if (compareSelected >= 2) {
+      if (compareSelected > 3) {
         $(this).attr('checked', false).parent().siblings(".maximum").toggle().addClass('js-active');
         $(this).closest('.package').removeClass('js-compare');
-      } else {
-      }
-    } else {
-    }
+      } else {}
+    } else {}
   } else {
-    if(compareSelected >= 1) {
-      if(compareSelected > 2) {
+    if (compareSelected >= 1) {
+      if (compareSelected > 2) {
         $(this).attr('checked', false).parent().siblings(".maximum").toggle().addClass('js-active');
         $(this).closest('.package').removeClass('js-compare');
-      } else {
-      }
-    } else {
-    }
+      } else {}
+    } else {}
   }
 });
 // End compare Button Maximum Alert
 
-
 // Compare Button Min Alert
 $(".compare-button").on("click", function() {
   var compareSelected = $(".comparePackage:checked").length;
-  if($(window).width() > 768){
-    if(compareSelected < 2) {
-        $(this).attr('checked', false).parent().siblings(".minimum").toggle().addClass('js-active');
-        return false;
-    } else {
-    }
-  } else {
-    if(compareSelected < 2) {
+  if ($(window).width() > 768) {
+    if (compareSelected < 2) {
       $(this).attr('checked', false).parent().siblings(".minimum").toggle().addClass('js-active');
       return false;
-    } else {
-    }
+    } else {}
+  } else {
+    if (compareSelected < 2) {
+      $(this).attr('checked', false).parent().siblings(".minimum").toggle().addClass('js-active');
+      return false;
+    } else {}
   }
 });
 // End compare Button Min Alert
@@ -102,16 +94,15 @@ $('.compareStartOver').click(function() {
 });
 // End close alert messages
 
-
 //  BYO JS - Expand / Collapse columns
-$(document).ready(function(){
-  if($(window).width() < 768){
+$(document).ready(function() {
+  if ($(window).width() < 768) {
     $('.byoHeader').siblings('.byoPackages').hide();
     $('.byoHeader').children("p").addClass("js-active");
   }
 });
-$(window).resize(function(){
-  if($(window).width() > 768){
+$(window).resize(function() {
+  if ($(window).width() > 768) {
     $('.byoHeader').siblings('.byoPackages').show();
   } else {
     $('.byoHeader').siblings('.byoPackages').hide();
@@ -119,7 +110,7 @@ $(window).resize(function(){
   }
 });
 $('.byoHeader').click(function() {
-  if($(window).width() < 769){
+  if ($(window).width() < 769) {
     console.log('tablet');
     $(this).siblings(".byoPackages").slideToggle();
     $(this).children("p").toggleClass("js-active");
@@ -129,12 +120,11 @@ $('.byoHeader').click(function() {
   }
 });
 
-
 //  End BYO JS - Expand / Collapse columns
 
 //  BYO Package Select
 $('.byoPackageSelect').click(function() {
-  if($(this).parents('.package').hasClass('js-selected')){
+  if ($(this).parents('.package').hasClass('js-selected')) {
     $(this).parents('.package').removeClass('js-selected');
     $(this).parents('.package').siblings('.package').removeClass('js-notSelected');
   } else {
@@ -147,7 +137,7 @@ $('.byoPackageSelect').click(function() {
 
 // Tab functionality
 jQuery(document).ready(function() {
-  jQuery('.psTabs .tab-links li').on('click', function(e)  {
+  jQuery('.psTabs .tab-links li').on('click', function(e) {
     var currentAttrValue = jQuery(this).children('span').data('tabs');
     // Show/Hide Tabs
     jQuery('#tab' + currentAttrValue).addClass('js-active').siblings().removeClass('js-active');
@@ -160,18 +150,18 @@ jQuery(document).ready(function() {
 
 // Prevent content from jumping below sticky header when it sticks
 // Must pair with CSS to hide/show .stickySpacing
-$(document).ready(function(){
+$(document).ready(function() {
   var stickySpacing = $('.sticky').outerHeight();
   $('.stickySpacing').height(stickySpacing);
 });
 // End prevent content from jumping below sticky header when it sticks
 
 // Expand / Collapse Select and Filter
-$(document).ready(function(){
+$(document).ready(function() {
   $('.collapse-button').parents('.stickyWrapper').addClass('js-open');
 });
 $('.collapse-button').click(function() {
-  if($(this).parents('.stickyWrapper').hasClass('js-open')){
+  if ($(this).parents('.stickyWrapper').hasClass('js-open')) {
     $(this).parents('.stickyWrapper').removeClass('js-open').addClass('js-closed');
     $(this).parents('.stickyWrapper').children('.package-filters').slideToggle();
   } else {
@@ -182,7 +172,6 @@ $('.collapse-button').click(function() {
 });
 // End expand / Collapse Select and Filter
 
-
 // Start Quick Checkout
 $('.quickCheckoutBtn').click(function() {
   $(this).parents('.quickCheckoutAccordion').toggleClass('js-open');
@@ -191,8 +180,8 @@ $('.quickCheckoutBtn').click(function() {
   return false;
 });
 
-$('.closeAccordion').click(function(){
-  if($(this).parents('.quickCheckoutAccordion').hasClass('js-open')) {
+$('.closeAccordion').click(function() {
+  if ($(this).parents('.quickCheckoutAccordion').hasClass('js-open')) {
     $(this).parents('.quickCheckoutContent').hide();
     $(this).parents('.quickCheckoutAccordion').removeClass('js-open');
     $(this).parents('.quickCheckoutAccordion').children('.quickCheckoutBtn').removeClass('icon-angle-up').addClass('icon-angle-down');
@@ -201,8 +190,3 @@ $('.closeAccordion').click(function(){
   }
 });
 // End Quick Checkout
-
-
-
-
-
