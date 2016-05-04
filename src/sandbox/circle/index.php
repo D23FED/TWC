766a6422b47b740ab6a9361b66621623330291e4
mehas <ethan.mehas@gmail.com>
2016-05-04 14:16:38 -0400

    Create circle progress example in CSS

---
+++ src/sandbox/circle/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/header.php');
+?>
+<div class="wrap">
+	<div class="container container-a">
+	  <div class="half-cirle"></div>
+	</div>
+	<div class="container container-b">
+	  <div class="half-cirle"></div>
+	</div>
+</div>
+<?php include_once($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/footer.php'); ?>
