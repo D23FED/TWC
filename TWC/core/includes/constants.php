4187c521ce39a5e0c4f89b00dda0765f4628afa8
mehas <ethan.mehas@gmail.com>
2016-05-17 09:33:11 -0400

    Small formatting changes

--- TWC/core/includes/constants.php
+++ TWC/core/includes/constants.php
@@ -1,6 +1,18 @@
 <?php
 // Constants
-define("ROOT", $_SERVER['DOCUMENT_ROOT']);
-define("CORE", ROOT . '/TWC/core/');
-define("CORE_INC", ROOT . '/TWC/core/includes/');
+$url = $_SERVER['HTTP_HOST'];
+$parsedUrl = parse_url($url);
+$host = explode('.', $parsedUrl['host']);
+$subdomain = $host[0];
+echo $_SERVER['HTTP_HOST'];
+echo 'loaded constants '.$subdomain;
+define("ROOT", $_SERVER['DOCUMENT_ROOT'].'/');
+if ( $subdomain != '' ) {
+	$urlBase = '';
+} else {
+	$urlBase = 'TWC';
+}
+define("CORE", ROOT . $urlBase . '/core/');
+define("CORE_INC", ROOT . $urlBase . '/core/includes/');
 ?>
+
