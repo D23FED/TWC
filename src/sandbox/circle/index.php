63b1cf651ae9215812e98b51cd5f4dfc466686bc
mehas <ethan.mehas@gmail.com>
2016-06-10 15:19:41 -0400

    Working version with final speedometer art, and multiple percentages. Prepping for release.

--- src/sandbox/circle/index.php
+++ src/sandbox/circle/index.php
@@ -1,15 +1,6 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/header.php');
 ?>
-<div class="wrapper" style="max-width: 980px;">
-<div class="hidden wrap circle-01">
-	<div class="container container-a">
-	  <div class="half-cirle"></div>
-	</div>
-	<div class="container container-b">
-	  <div class="half-cirle"></div>
-	</div>
-</div>
 
 
 <div class="speedometer">
@@ -17,10 +8,112 @@ include_once($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/header.php');
     <div class="dial">
       <div class="bar">
         <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed"></div>
+        <div class="half-circle half-circle-speed animated"></div>
         <div class="half-circle half-circle-cover"></div>
       </div>
-      <div class="needle">
+      <div class="needle animated">
+        <div class="inner">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="speedometer speedometer-66">
+  <div class="wrap">
+    <div class="dial">
+      <div class="bar">
+        <div class="half-circle half-circle-bg"></div>
+        <div class="half-circle half-circle-speed animated"></div>
+        <div class="half-circle half-circle-cover"></div>
+      </div>
+      <div class="needle animated">
+        <div class="inner">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="speedometer speedometer-60">
+  <div class="wrap">
+    <div class="dial">
+      <div class="bar">
+        <div class="half-circle half-circle-bg"></div>
+        <div class="half-circle half-circle-speed animated"></div>
+        <div class="half-circle half-circle-cover"></div>
+      </div>
+      <div class="needle animated">
+        <div class="inner">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="speedometer speedometer-40">
+  <div class="wrap">
+    <div class="dial">
+      <div class="bar">
+        <div class="half-circle half-circle-bg"></div>
+        <div class="half-circle half-circle-speed animated"></div>
+        <div class="half-circle half-circle-cover"></div>
+      </div>
+      <div class="needle animated">
+        <div class="inner">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="speedometer speedometer-33">
+  <div class="wrap">
+    <div class="dial">
+      <div class="bar">
+        <div class="half-circle half-circle-bg"></div>
+        <div class="half-circle half-circle-speed animated"></div>
+        <div class="half-circle half-circle-cover"></div>
+      </div>
+      <div class="needle animated">
+        <div class="inner">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="speedometer speedometer-30">
+  <div class="wrap">
+    <div class="dial">
+      <div class="bar">
+        <div class="half-circle half-circle-bg"></div>
+        <div class="half-circle half-circle-speed animated"></div>
+        <div class="half-circle half-circle-cover"></div>
+      </div>
+      <div class="needle animated">
+        <div class="inner">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="speedometer speedometer-16">
+  <div class="wrap">
+    <div class="dial">
+      <div class="bar">
+        <div class="half-circle half-circle-bg"></div>
+        <div class="half-circle half-circle-speed animated"></div>
+        <div class="half-circle half-circle-cover"></div>
+      </div>
+      <div class="needle animated">
         <div class="inner">
           <div class="top"></div>
           <div class="bottom"></div>
@@ -31,11 +124,6 @@ include_once($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/header.php');
 </div>
 <div class="controls">
 </div>
-
-<div class="wrap">
-	<div class="dial"></div>
-</div>
-
 </div>
 <script>
 
