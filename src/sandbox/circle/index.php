dc63a9b5b446faa5a83380ef344adfc670a1b25a
mehas <ethan.mehas@gmail.com>
2016-06-13 15:03:04 -0400

    * Create static and animated versions * Create small versions * Allow animation to be triggered via JS, when modal is launched

--- src/sandbox/circle/index.php
+++ src/sandbox/circle/index.php
@@ -2,130 +2,137 @@
 include_once($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/header.php');
 ?>
 
-
-<div class="speedometer">
-  <div class="wrap">
-    <div class="dial">
-      <div class="bar">
-        <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed animated"></div>
-        <div class="half-circle half-circle-cover"></div>
-      </div>
-      <div class="needle animated">
-        <div class="inner">
-          <div class="top"></div>
-          <div class="bottom"></div>
+<div class="percent-100">
+  <div class="speedometer">
+    <div class="wrap">
+      <div class="dial">
+        <div class="bar">
+          <div class="half-circle half-circle-bg"></div>
+          <div class="half-circle half-circle-speed animated"></div>
+          <div class="half-circle half-circle-cover"></div>
+        </div>
+        <div class="needle animated">
+          <div class="inner">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="speedometer speedometer-66">
-  <div class="wrap">
-    <div class="dial">
-      <div class="bar">
-        <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed animated"></div>
-        <div class="half-circle half-circle-cover"></div>
-      </div>
-      <div class="needle animated">
-        <div class="inner">
-          <div class="top"></div>
-          <div class="bottom"></div>
+<div class="percent-66">
+  <div class="speedometer">
+    <div class="wrap">
+      <div class="dial">
+        <div class="bar">
+          <div class="half-circle half-circle-bg"></div>
+          <div class="half-circle half-circle-speed animated"></div>
+          <div class="half-circle half-circle-cover"></div>
+        </div>
+        <div class="needle animated">
+          <div class="inner">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="speedometer speedometer-60">
-  <div class="wrap">
-    <div class="dial">
-      <div class="bar">
-        <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed animated"></div>
-        <div class="half-circle half-circle-cover"></div>
-      </div>
-      <div class="needle animated">
-        <div class="inner">
-          <div class="top"></div>
-          <div class="bottom"></div>
+<div class="percent-60">
+  <div class="speedometer">
+    <div class="wrap">
+      <div class="dial">
+        <div class="bar">
+          <div class="half-circle half-circle-bg"></div>
+          <div class="half-circle half-circle-speed animated"></div>
+          <div class="half-circle half-circle-cover"></div>
+        </div>
+        <div class="needle animated">
+          <div class="inner">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="speedometer speedometer-40">
-  <div class="wrap">
-    <div class="dial">
-      <div class="bar">
-        <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed animated"></div>
-        <div class="half-circle half-circle-cover"></div>
-      </div>
-      <div class="needle animated">
-        <div class="inner">
-          <div class="top"></div>
-          <div class="bottom"></div>
+<div class="percent-40">
+  <div class="speedometer">
+    <div class="wrap">
+      <div class="dial">
+        <div class="bar">
+          <div class="half-circle half-circle-bg"></div>
+          <div class="half-circle half-circle-speed animated"></div>
+          <div class="half-circle half-circle-cover"></div>
+        </div>
+        <div class="needle animated">
+          <div class="inner">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="speedometer speedometer-33">
-  <div class="wrap">
-    <div class="dial">
-      <div class="bar">
-        <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed animated"></div>
-        <div class="half-circle half-circle-cover"></div>
-      </div>
-      <div class="needle animated">
-        <div class="inner">
-          <div class="top"></div>
-          <div class="bottom"></div>
+<div class="percent-33">
+  <div class="speedometer">
+    <div class="wrap">
+      <div class="dial">
+        <div class="bar">
+          <div class="half-circle half-circle-bg"></div>
+          <div class="half-circle half-circle-speed animated"></div>
+          <div class="half-circle half-circle-cover"></div>
+        </div>
+        <div class="needle animated">
+          <div class="inner">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="speedometer speedometer-30">
-  <div class="wrap">
-    <div class="dial">
-      <div class="bar">
-        <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed animated"></div>
-        <div class="half-circle half-circle-cover"></div>
-      </div>
-      <div class="needle animated">
-        <div class="inner">
-          <div class="top"></div>
-          <div class="bottom"></div>
+<div class="percent-30">
+  <div class="speedometer">
+    <div class="wrap">
+      <div class="dial">
+        <div class="bar">
+          <div class="half-circle half-circle-bg"></div>
+          <div class="half-circle half-circle-speed animated"></div>
+          <div class="half-circle half-circle-cover"></div>
+        </div>
+        <div class="needle animated">
+          <div class="inner">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="speedometer speedometer-16">
-  <div class="wrap">
-    <div class="dial">
-      <div class="bar">
-        <div class="half-circle half-circle-bg"></div>
-        <div class="half-circle half-circle-speed animated"></div>
-        <div class="half-circle half-circle-cover"></div>
-      </div>
-      <div class="needle animated">
-        <div class="inner">
-          <div class="top"></div>
-          <div class="bottom"></div>
+<div class="percent-16">
+  <div class="speedometer">
+    <div class="wrap">
+      <div class="dial">
+        <div class="bar">
+          <div class="half-circle half-circle-bg"></div>
+          <div class="half-circle half-circle-speed animated"></div>
+          <div class="half-circle half-circle-cover"></div>
+        </div>
+        <div class="needle animated">
+          <div class="inner">
+            <div class="top"></div>
+            <div class="bottom"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="controls">
-</div>
-</div>
-<script>
-
-</script>
 <?php include_once($_SERVER['DOCUMENT_ROOT']. '/TWC/core/includes/footer.php'); ?>
