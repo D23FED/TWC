9e0d434f2c6b894b0739cb4f47528f3767dd2858
mehas <ethan.mehas@gmail.com>
2016-06-30 15:15:21 -0400

    First working version of carousel sticky nav

---
+++ src/sandbox/sticky-nav-premium/components.php
@@ -0,0 +1,44 @@
+<div class="manual-id-wrapper section">
+  <div id="sticky-subnav-wrapper">
+    <div class="parsys">
+      <div class="manual-id-wrapper section">
+        <div id="sticky-subnav">
+          <div class="parsys">
+            <div class="twc-container section">
+              <div class="icon-none cta twc-icon-before section noButton">
+                <a analyticsname="#:1:1:international" href="#international" id="6f387d65-7115-4630-821a-856bf635677f">
+                  <span>International</span>
+                </a>
+                <div class="clear"></div>
+              </div>
+              <div class="icon-none cta twc-icon-before section noButton">
+                <a analyticsname="#:1:2:premiums" href="#premiums" id="7b6052a0-1f36-4090-9d46-d85da0019fad">
+                  <span>Premiums</span>
+                </a>
+                <div class="clear"></div>
+              </div>
+              <div class="icon-none cta twc-icon-before section noButton">
+                <a analyticsname="#:1:3:sports" href="#sports" id="0c3ad0b1-7521-47c8-a55f-1efea509afc2">
+                  <span>Sports</span>
+                </a>
+                <div class="clear"></div>
+              </div>
+              <div class="icon-none cta twc-icon-before section noButton">
+                <a analyticsname="#:1:4:adult" href="#adult" id="c642bcd7-4546-4fec-a0b4-fb5160a55324">
+                  <span>Adult</span>
+                </a>
+                <div class="clear"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="manual-id-wrapper section">
+        <div id="sticky-spacer" style="height: 62px; display: none;">
+          <div class="parsys">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
